85bc9f60056af7589961ea6fa62bf7190b67658c
Luiz <luizguilherme.lgrl@gmail.com>
2022-07-04 15:04:34 -0300

    ADJUSTING REPOSITORY AND FIND METHOD

--- src/main/java/br/com/luizgrl/projeto/resources/CategoriaResource.java
+++ src/main/java/br/com/luizgrl/projeto/resources/CategoriaResource.java
@@ -1,23 +1,23 @@
 package br.com.luizgrl.projeto.resources;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.luizgrl.projeto.domain.Categoria;
+import br.com.luizgrl.projeto.service.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias") //end padrão rest 
 public class CategoriaResource {
-    @RequestMapping(method = RequestMethod.GET) // Como é uma operação para se obter dados usa o verbo GET
-    public List<Categoria> listing(){
-        Categoria cat = new Categoria();
-        Categoria cat2 = new Categoria();
-        List<Categoria> lista = new ArrayList<>();
-        lista.add(cat);
-        lista.add(cat2);
-        return lista;
+
+    @Autowired
+    private CategoriaService categoriaService;
+    @RequestMapping(value ="/{id}" /*definindo end point */, method = RequestMethod.GET) // Como é uma operação para se obter dados usa o verbo GET
+    public ResponseEntity<?> find(@PathVariable /* usado para dizer que o id da url deve ir para a variavel */Integer id){
+        Categoria obj = categoriaService.find(id);
+        return ResponseEntity.ok().body(obj); // vai dar a reposta ok passando no corpo do return o objeto obj
     }
     
 }
