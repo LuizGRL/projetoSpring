0b5d1782255dbdaab589251f06b51970bb955fcb
Luiz <luizguilherme.lgrl@gmail.com>
2022-07-05 13:03:22 -0300

    FINISHING CATEGORIA

--- src/main/java/br/com/luizgrl/projeto/ProjetoApplication.java
+++ src/main/java/br/com/luizgrl/projeto/ProjetoApplication.java
@@ -1,13 +1,32 @@
 package br.com.luizgrl.projeto;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.luizgrl.projeto.domain.Categoria;
+import br.com.luizgrl.projeto.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetoApplication {
+public class ProjetoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria categoria1 = new Categoria(null,"escritorio");
+		Categoria categoria2 = new Categoria(null,"Computador");	
+
+		categoriaRepository.saveAll(Arrays.asList(categoria1,categoria2));
+
 	}
 
 }
