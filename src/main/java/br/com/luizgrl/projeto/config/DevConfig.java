0d8525c65bf8a7802ac2d1ad278f12dc1b03a284
Luiz <luizguilherme.lgrl@gmail.com>
2022-07-18 16:48:51 -0300

    test

---
+++ src/main/java/br/com/luizgrl/projeto/config/DevConfig.java
@@ -0,0 +1,28 @@
+package br.com.luizgrl.projeto.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.luizgrl.projeto.service.DBService;
+
+@Configuration // para ser reconhecido como arqruivo de teste 
+@Profile("dev") // so vai permitir os bean serem usados quando o perfil de teste estiver ativo 
+public class DevConfig {
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException{
+        dbService.instantiateTestDatabase();
+
+        return true;
+
+    }
+
+
+    
+}
