4ca77d292be857d06f2e3943556be44b875103b8
Luiz <luizguilherme.lgrl@gmail.com>
2022-07-20 13:06:55 -0300

    CREATING A DEV PROFILE

--- src/main/java/br/com/luizgrl/projeto/config/DevConfig.java
+++ src/main/java/br/com/luizgrl/projeto/config/DevConfig.java
@@ -3,6 +3,7 @@ package br.com.luizgrl.projeto.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,9 +15,15 @@ import br.com.luizgrl.projeto.service.DBService;
 public class DevConfig {
     @Autowired
     private DBService dbService;
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
 
     @Bean
     public boolean instantiateDatabase() throws ParseException{
+
+        if(!"create".equals(strategy)){
+            return  false;
+        }
         dbService.instantiateTestDatabase();
 
         return true;
